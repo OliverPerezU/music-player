ed7af315850734ca7c0def2914dfe1f807c9831e
Revil <operezl12@miumg.edu.gt>
2025-06-14 12:30:35 -0600

    feat(input method): Added data entry methods

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Song {
     String title;
     String artist;
@@ -75,6 +77,35 @@ public class App {
         System.out.println("5. Eliminar canción");
         System.out.println("6. Salir");
         System.out.println("Selecciones la opción que desea realizar");
+    }
+
+    public static int numEntry(Scanner scanner) {
+        try {
+            int number = Integer.parseInt(scanner.nextLine().trim());
+            if (number <= 0) {
+                System.out.println("Por favor ingresa un número mayor que 0");
+                return numEntry(scanner);
+            }
+            return number;
+        } catch (NumberFormatException e) {
+            System.out.println("Por favor ingresa un número válido");
+            return numEntry(scanner);
+        }
+    }
+
+    public static String stringEntry(Scanner scanner) {
+        try {
+            String input = scanner.nextLine().trim();
+            if (input.isEmpty()) {
+                System.out.println("Por favor ingresa un texto válido");
+                return stringEntry(scanner);
+            }
+            return input;
+
+        } catch (Exception e) {
+            System.out.println("Ocurrió un error al leer la entrada. Por favor, inténtalo de nuevo.");
+            return stringEntry(scanner);
+        }
     }
 
     public static void main(String[] args) throws Exception {
