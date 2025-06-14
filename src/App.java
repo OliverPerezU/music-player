490225e081bb8f52ad91dae6d7f4c620aafd447c
Revil <operezl12@miumg.edu.gt>
2025-06-14 12:19:13 -0600

    feat(song): Added class for song

--- src/App.java
+++ src/App.java
@@ -1,3 +1,21 @@
+class Song {
+    String title;
+    String artist;
+    int time;
+
+    public Song(String title, String artist, int time) {
+        this.title = title;
+        this.artist = artist;
+        this.time = time;
+    }
+
+    public void details() {
+        System.out.println("Título:" + title);
+        System.out.println("Artista:" + artist);
+        System.out.println("Duración:" + time + " segundos");
+    }
+}
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
